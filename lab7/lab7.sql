1ffae0fb9cc5623b802e5135c04eec3ffb432923
Ilya Protasov <pfltriot@gmail.com>
2018-05-18 09:19:37 +0300

    added lab7

--- lab7/lab7.sql
+++ lab7/lab7.sql
@@ -74,9 +74,109 @@ CREATE TABLE IF NOT EXISTS `grade` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
--- 2)	Выдать оценки студентов по математике если они обучаются/обучались данному предмету. Оформить выдачу данных с использованием view.
+-- 2)	Выдать оценки студентов по математике если они обучаются/обучались данному предмету.
+-- Оформить выдачу данных с использованием view.
+CREATE VIEW math_marks AS
+    SELECT student.surname, grade.mark
+    FROM `grade`
+      LEFT JOIN `lesson` ON grade.id_lesson = lesson.id_lesson
+      LEFT JOIN `student` ON grade.id_student = student.id_student
+    WHERE lesson.id_subject = 10;
+
+SELECT * FROM math_marks;
+
+
 -- 3)	Дать информацию о должниках с указанием фамилии студента и названия предмета.
--- Должниками считаются студенты, не имеющие оценки по предмету, который ведется в группе. Оформить в виде процедуры, на вход название группы.
--- 4)	Дать среднюю оценку студентов по каждому предмету для тех предметов, по которым занимается не менее 10 студентов.
--- 5)	Дать оценки студентов специальности ВМ по всем проводимым предметам с указанием группы, фамилии, предмета, даты. При отсутствии оценки заполнить значениями NULL поля оценки и даты.
--- 6)	Всем студентам специальности ИВТ, получившим оценки меньшие 5 по предмету БД до 12.05, повысить эти оценки на 1 балл.
+-- Должниками считаются студенты, не имеющие оценки по предмету, который ведется в группе.
+-- Оформить в виде процедуры, на вход название группы.
+DROP PROCEDURE IF EXISTS  showDebtors;
+DELIMITER //
+CREATE PROCEDURE showDebtors (IN groupName VARCHAR(100))
+  BEGIN
+    DROP TABLE IF EXISTS `group_debtor`;
+    CREATE TEMPORARY TABLE `group_debtor` AS (
+      SELECT
+        student.id_student AS id_student,
+        student.surname as st_surname,
+        subject.name AS sb_name,
+        lesson.id_lesson AS id_lesson
+      FROM `lesson`
+        LEFT JOIN `subject` ON subject.id_subject = lesson.id_subject
+        LEFT JOIN `group` ON `group`.id_group = lesson.id_group
+        LEFT JOIN `student` ON `group`.id_group = student.id_group
+      WHERE
+        `group`.short_name = groupName
+    );
+
+    SELECT DISTINCT
+      group_debtor.st_surname,
+      group_debtor.sb_name
+    FROM `group_debtor`
+      LEFT JOIN `grade` ON grade.id_student = group_debtor.id_student AND grade.id_lesson = group_debtor.id_lesson
+    GROUP BY group_debtor.st_surname, group_debtor.sb_name
+      HAVING COUNT(grade.mark) = 0;
+    DROP TABLE IF EXISTS `group_debtor`;
+  END //
+DELIMITER ;
+
+CALL showDebtors('ИВТ-21');
+CALL showDebtors('ВМ-22');
+
+
+
+
+-- 4)	Дать среднюю оценку студентов по каждому предмету для тех предметов,
+-- по которым занимается не менее 10 студентов.
+DROP TABLE IF EXISTS `#avg_students_mark`;
+CREATE TEMPORARY TABLE IF NOT EXISTS `#avg_students_mark` AS (
+  SELECT
+    lesson.id_subject       AS id_sub,
+    COUNT(grade.id_student) AS student_amount,
+    AVG(grade.mark)         AS avg_mark
+  FROM `lesson`
+    LEFT JOIN `subject` ON subject.id_subject = lesson.id_subject
+    LEFT JOIN `grade` ON grade.id_lesson = lesson.id_lesson
+  GROUP BY lesson.id_subject
+  HAVING student_amount >= 6
+);
+
+SELECT `#avg_students_mark`.id_sub, subject.name AS name_sub, `#avg_students_mark`.avg_mark
+FROM `#avg_students_mark`
+  LEFT JOIN `subject` ON subject.id_subject = `#avg_students_mark`.id_sub;
+
+DROP TABLE `#avg_students_mark`;
+
+-- 5)	Дать оценки студентов специальности ВМ по всем проводимым предметам с указанием группы,
+-- фамилии, предмета, даты. При отсутствии оценки заполнить значениями NULL поля оценки и даты.
+DROP TABLE IF EXISTS `avg_students_mark`;
+CREATE TEMPORARY TABLE IF NOT EXISTS `avg_students_mark` AS (
+  SELECT
+    student.id_student AS id_student,
+    student.surname AS student_name,
+    `group`.short_name AS group_name,
+    `subject`.name AS subject_name,
+    lesson.date AS lesson_date
+  FROM `lesson`
+    LEFT JOIN `subject` ON subject.id_subject = lesson.id_subject
+    LEFT JOIN `group` ON `group`.id_group = lesson.id_group
+    LEFT JOIN `student` ON student.id_group = `group`.id_group
+  WHERE `group`.short_name_of_speciality = 'ВМ'
+);
+
+SELECT `avg_students_mark`.student_name, `avg_students_mark`.group_name, `avg_students_mark`.subject_name, `avg_students_mark`.lesson_date, grade.mark
+FROM `grade`
+  RIGHT JOIN `avg_students_mark` ON `avg_students_mark`.id_student = grade.id_student;
+
+DROP TABLE `avg_students_mark`;
+
+-- 6)	Всем студентам специальности ИВТ, получившим оценки меньшие 5 по предмету БД до 12.05,
+-- повысить эти оценки на 1 балл.
+UPDATE `grade`
+  LEFT JOIN `lesson` ON lesson.id_lesson = grade.id_lesson
+  LEFT JOIN `group` ON `group`.id_group = lesson.id_group
+  LEFT JOIN `student` ON student.id_group = `group`.id_group
+SET grade.mark = grade.mark + 1
+WHERE `group`.short_name_of_speciality = 'ИВТ'
+      AND lesson.id_subject = 1
+      AND lesson.date < '2017-05-12'
+      AND grade.mark < 5;
